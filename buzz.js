88556b52020272b8e706531f3d77f6c299b7d633
Antoine Turmel <geekshadow@gmail.com>
2013-06-09 17:33:37 +0200

    added manifest, install and icons

--- buzz.js
+++ buzz.js
@@ -12,3 +12,8 @@ function buzzaction(){
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function installme(){
+  var manifestURL = "http://geekshadow.github.io/buzzlightyear/manifest.webapp";
+  navigator.mozApps.install(manifestURL);
+}
